0acfa6be401f941b072451d35c8e044cf7f8a7b9
nyiwaingchit7 <nyiwaingchit5@gmail.com>
2024-05-30 11:37:14 +0630

    starting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
