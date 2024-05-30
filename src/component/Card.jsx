3393c52dde6d88435da0df3b9e0c4f22b4ae88b9
nyiwaingchit7 <nyiwaingchit5@gmail.com>
2024-05-30 21:26:22 +0630

    card-ui

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Card = () => {
-  return <div></div>;
+const Card = ({ data }) => {
+  const [hoverEffect, setHoverEffect] = useState(false);
+  return (
+    <Link
+      to={`/detail/${data.Id}`}
+      className=" flex-shrink-0"
+      onMouseOver={() => setHoverEffect(true)}
+      onMouseLeave={() => setHoverEffect(false)}
+    >
+      <div className=" border-4 border-[#FFFFFF]  relative ">
+        <img
+          src={`img/${data.Id}.jpg`}
+          className={`${
+            hoverEffect ? "hidden" : "w-[225px] h-[250px] object-cover "
+          }`}
+          alt="pic"
+        />
+        {!hoverEffect && (
+          <div className=" absolute top-0 w-[225px] h-[250px]  flex justify-center items-center">
+            <p className="text-[#FFFFFF]  text-[1.5rem] font-bold text-center">
+              {data.MonthMm}
+            </p>
+          </div>
+        )}
+        <div
+          className={`${
+            hoverEffect
+              ? "w-[225px] h-[250px] bg-[#ffffff] flex justify-center items-center "
+              : "hidden"
+          }`}
+        >
+          <p className="text-[1.5rem] font-bold text-center"> {data.MonthMm}</p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Card;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { monthData } from "../utils/data";
+import Card from "./Card";
 
 const Home = () => {
-  return <div className=""></div>;
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const element = elementRef.current;
+
+    const onWheel = (event) => {
+      if (event.deltaY !== 0) {
+        element.scrollLeft += event.deltaY * 10;
+        event.preventDefault();
+      }
+    };
+
+    if (element) {
+      element.addEventListener("wheel", onWheel);
+    }
+  }, []);
+  return (
+    <div
+      ref={elementRef}
+      className=" max-w-[1000px] mx-auto overflow-auto scroll-smooth scrollbar-none "
+    >
+      <div className="h-[100vh] flex items-center gap-5 ">
+        {monthData.map((d) => (
+          <Card data={d} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
